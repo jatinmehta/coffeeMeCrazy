ee8121949b79707b22be142351146d279807b80a
Jatin Mehta <mehtajatin05@gmail.com>
2014-01-10 23:58:25 +0530

    Update coffeebeans.js

--- public/javascripts/coffeebeans.js
+++ public/javascripts/coffeebeans.js
@@ -1,19 +1,12 @@
 var viewModel = {
-  flash: ko.observable(),
   shownOnce: ko.observable(),
-  currentPage: ko.observable(),
+  pageToDisplay: ko.observable(),
   errors: ko.observableArray(),
-  items: ko.observableArray(),
-  comments: ko.observableArray(),
+  successMsg: ko.observable(),
+  beans: ko.observableArray(),
   selectedItem: ko.observable(),
-  newcontent: ko.observable(),
- beanid: ko.observable(0),
-  filter: ko.observable(""),
-  tempName: ko.observable(),
-  tempDescription: ko.observable(),
-  tempPlace: ko.observable(),
-
-  tempItem: {
+  
+  tempBean: {
     id: ko.observable(),
     name: ko.observable(),
     description: ko.observable(),
@@ -22,143 +15,48 @@ var viewModel = {
     updated_at: ko.observable(),
     created_at: ko.observable()
   },
-
+  
+  tempBeanId: ko.observable(0),
+  comments: ko.observableArray(),
+  newCommentText: ko.observable(),
   newcomment: {
     coffeebean_id: "",
     content: ""
   },
-
+  
+  tempBeanName: ko.observable(),
+  tempBeanDesc: ko.observable(),
+  tempBeanPlace: ko.observable(),
   newBean: {
     name: "",
     description: "",
     place: "",
     likes: 0
   },
+  
+  filterBeans: ko.observable(""),
 
-  setFlash: function(flash) {
-    this.flash(flash);
+  setSuccessMsg: function(successMsg) {
+    this.successMsg(successMsg);
     this.shownOnce(false);
   },
 
-  checkFlash: function() {
+  checkSuccessMsg: function() {
     if (this.shownOnce() == true) {
-      this.flash('');
+      this.successMsg('');
     }
   },
   
-  clearTempItem: function() {
-    this.tempItem.id('');
-    this.tempItem.name('');
-    this.tempItem.description('');
-    this.tempItem.place('');
-    this.tempItem.likes('');
-    this.tempItem.updated_at('');
-    this.tempItem.created_at('');
-  },
-
-  clearComments: function() {
-    this.comments('');
+  prepareTempBean : function() {
+    this.tempBean.id(ko.utils.unwrapObservable(this.selectedItem().id));
+    this.tempBean.name(ko.utils.unwrapObservable(this.selectedItem().name));
+    this.tempBean.description(ko.utils.unwrapObservable(this.selectedItem().description));
+    this.tempBean.place(ko.utils.unwrapObservable(this.selectedItem().place));
+    this.tempBean.likes(ko.utils.unwrapObservable(this.selectedItem().likes));
+    this.tempBean.updated_at(ko.utils.unwrapObservable(this.selectedItem().updated_at));
+    this.tempBean.created_at(ko.utils.unwrapObservable(this.selectedItem().created_at));
   },
   
-  prepareTempItem : function() {
-    this.tempItem.id(ko.utils.unwrapObservable(this.selectedItem().id));
-    this.tempItem.name(ko.utils.unwrapObservable(this.selectedItem().name));
-    this.tempItem.description(ko.utils.unwrapObservable(this.selectedItem().description));
-    this.tempItem.place(ko.utils.unwrapObservable(this.selectedItem().place));
-    this.tempItem.likes(ko.utils.unwrapObservable(this.selectedItem().likes));
-    this.tempItem.updated_at(ko.utils.unwrapObservable(this.selectedItem().updated_at));
-    this.tempItem.created_at(ko.utils.unwrapObservable(this.selectedItem().created_at));
-  },
-  
-  indexBean: function() {
-    this.checkFlash();
-    this.errors([]);
-    this.flash('');
-    this.clearComments();
-    $.getJSON('/coffeebeans.json', function(data) {
-      viewModel.items(data);
-      viewModel.currentPage('index');
-      viewModel.shownOnce(true);
-    });
-  },
-
-  showBean: function(itemToShow) {
-    this.checkFlash();
-    this.errors([]);
-    this.newcontent('');
-    this.selectedItem(itemToShow);
-    this.prepareTempItem();
-    var url = '/coffeebeans/' + itemToShow.id + '/comments.json';
-    $.getJSON(url, function(data) {
-      viewModel.comments(data);
-    });
-    this.currentPage('show');
-    this.shownOnce(true);
-    this.beanid(itemToShow.id);
-  },
-
-
-  editBean: function(itemToEdit) {
-    this.checkFlash();
-    this.selectedItem(itemToEdit);
-    this.prepareTempItem();
-    this.currentPage('edit');
-    this.shownOnce(true);
-  },
-
-  addBean: function() {
-    this.newBean.name = this.tempName();
-    this.newBean.description = this.tempDescription();
-    this.newBean.place = this.tempPlace();
-    var json_data = ko.toJS(this.newBean);
-    
-    $.ajax({
-      type: 'POST',
-      url: '/coffeebeans.json',
-      data: {
-        // /// 17
-        coffeebean: json_data
-      },
-      dataType: "json",
-      success: function(createdItem) {
-        viewModel.errors([]);
-      
-        viewModel.items.push(viewModel.newBean);
-        viewModel.tempName('');
-        viewModel.tempDescription('');
-        viewModel.tempPlace('');
-      },
-      error: function(msg) {
-        viewModel.errors(JSON.parse(msg.responseText));
-      }
-    });
-  },
-
-  addComment: function() {
-    this.newcomment.coffeebean_id = this.beanid();
-    this.newcomment.content = this.newcontent();
-    var json_data = ko.toJS(this.newcomment);
-    
-    $.ajax({
-      type: 'POST',
-      url: '/coffeebeans/' + this.beanid() + '/comments.json',
-      data: {
-        // /// 17
-        comment: json_data
-      },
-      dataType: "json",
-      success: function(createdItem) {
-        viewModel.errors([]);
-      
-        viewModel.comments.push(viewModel.newcomment);
-        viewModel.newcontent('');
-      },
-      error: function(msg) {
-        viewModel.errors(JSON.parse(msg.responseText));
-      }
-    });
-  },
-  // /// 16
   createBean: function(itemToCreate) {
     var json_data = ko.toJS(itemToCreate);
     $.ajax({
@@ -171,34 +69,94 @@ var viewModel = {
       dataType: "json",
       success: function(createdItem) {
         viewModel.errors([]);
-        viewModel.setFlash('New Coffee Bean Added.');
-        viewModel.showBean(createdItem);
+        viewModel.setSuccessMsg('New Coffee Bean Added.');
+        viewModel.beanDisplay(createdItem);
       },
       error: function(msg) {
         viewModel.errors(JSON.parse(msg.responseText));
       }
     });
   },
+  
+  beanDisplay: function(beanToShow) {
+    this.checkSuccessMsg();
+    this.errors([]);
+    this.newCommentText('');
+    this.selectedItem(beanToShow);
+    this.prepareTempBean();
+    var url = '/coffeebeans/' + beanToShow.id + '/comments.json';
+    $.getJSON(url, function(data) {
+      viewModel.comments(data);
+    });
+    this.pageToDisplay('show');
+    this.shownOnce(true);
+    this.tempBeanId(beanToShow.id);
+  },
 
-  tryBean: function(itemToUpdate) {
-    itemToUpdate.likes(itemToUpdate.likes() + 1);
-    var temp = 0;
-    var json_data = ko.toJS(itemToUpdate);
+
+  editBean: function(itemToEdit) {
+    this.checkSuccessMsg();
+    this.selectedItem(itemToEdit);
+    this.prepareTempBean();
+    this.pageToDisplay('edit');
+    this.shownOnce(true);
+  },
+
+  updateBean: function(beanToUpdate) {
+    var json_data = ko.toJS(beanToUpdate);
     delete json_data.id;
     delete json_data.created_at;
     delete json_data.updated_at;
 
     $.ajax({
       type: 'PUT',
-      url: '/coffeebeans/' + itemToUpdate.id() + '.json',
+      url: '/coffeebeans/' + beanToUpdate.id() + '.json',
       data: {
         coffeebean: json_data
       },
       dataType: "json",
-      success: function(updatedItem) {
+      success: function(updatedBean) {
+        viewModel.errors([]);
+
+        viewModel.beanDisplay(updatedBean);
+      },
+      error: function(msg) {
+        viewModel.errors(JSON.parse(msg.responseText));
+      }
+    });
+  },
+  
+  indexBean: function() {
+    this.checkSuccessMsg();
+    this.errors([]);
+    this.successMsg('');
+    this.clearComments();
+    $.getJSON('/coffeebeans.json', function(data) {
+      viewModel.beans(data);
+      viewModel.pageToDisplay('index');
+      viewModel.shownOnce(true);
+    });
+  },
+
+  loveBean: function(beanToUpdate) {
+    beanToUpdate.likes(beanToUpdate.likes() + 1);
+    var temp = 0;
+    var json_data = ko.toJS(beanToUpdate);
+    delete json_data.id;
+    delete json_data.created_at;
+    delete json_data.updated_at;
+
+    $.ajax({
+      type: 'PUT',
+      url: '/coffeebeans/' + beanToUpdate.id() + '.json',
+      data: {
+        coffeebean: json_data
+      },
+      dataType: "json",
+      success: function(updatedBean) {
         viewModel.errors([]);
         
-        viewModel.showAction(updatedItem);
+        viewModel.showAction(updatedBean);
         this.temp(1);
       },
       error: function(msg) {
@@ -207,36 +165,80 @@ var viewModel = {
     });  
 
   },
-  // /// 18
-  updateBean: function(itemToUpdate) {
-    var json_data = ko.toJS(itemToUpdate);
-    delete json_data.id;
-    delete json_data.created_at;
-    delete json_data.updated_at;
-
+  
+  addBean: function() {
+    this.newBean.name = this.tempBeanName();
+    this.newBean.description = this.tempBeanDesc();
+    this.newBean.place = this.tempBeanPlace();
+    var json_data = ko.toJS(this.newBean);
+    
     $.ajax({
-      type: 'PUT',
-      url: '/coffeebeans/' + itemToUpdate.id() + '.json',
+      type: 'POST',
+      url: '/coffeebeans.json',
       data: {
+        
         coffeebean: json_data
       },
       dataType: "json",
-      success: function(updatedItem) {
+      success: function(createdItem) {
         viewModel.errors([]);
-
-        viewModel.showBean(updatedItem);
+      
+        viewModel.beans.push(viewModel.newBean);
+        viewModel.tempBeanName('');
+        viewModel.tempBeanDesc('');
+        viewModel.tempBeanPlace('');
       },
       error: function(msg) {
         viewModel.errors(JSON.parse(msg.responseText));
       }
     });
   },
-  // /// 19
-  destroyBean: function(itemToDestroy) {
+
+  addComment: function() {
+    this.newcomment.coffeebean_id = this.tempBeanId();
+    this.newcomment.content = this.newCommentText();
+    var json_data = ko.toJS(this.newcomment);
+    
+    $.ajax({
+      type: 'POST',
+      url: '/coffeebeans/' + this.tempBeanId() + '/comments.json',
+      data: {
+        
+        comment: json_data
+      },
+      dataType: "json",
+      success: function(createdItem) {
+        viewModel.errors([]);
+      
+        viewModel.comments.push(viewModel.newcomment);
+        viewModel.newCommentText('');
+      },
+      error: function(msg) {
+        viewModel.errors(JSON.parse(msg.responseText));
+      }
+    });
+  },
+  
+    
+  clearTempItem: function() {
+    this.tempBean.id('');
+    this.tempBean.name('');
+    this.tempBean.description('');
+    this.tempBean.place('');
+    this.tempBean.likes('');
+    this.tempBean.updated_at('');
+    this.tempBean.created_at('');
+  },
+
+  clearComments: function() {
+    this.comments('');
+  },
+  
+  beanDelete: function(beanToDelete) {
     if (confirm('Are you sure?')) {
       $.ajax({
         type: "DELETE",
-        url: '/coffeebeans/' + itemToDestroy.id + '.json',
+        url: '/coffeebeans/' + beanToDelete.id + '.json',
         dataType: "json",
         success: function(){
           viewModel.errors([]);
@@ -258,13 +260,13 @@ ko.utils.stringStartsWith = function (string, startsWith) {
   return string.substring(0, startsWith.length) === startsWith;
 };
 
-viewModel.filteredItems = ko.computed(function() {
-    var filter = this.filter().toLowerCase();
-    if (!filter) {
-        return this.items();
+viewModel.filteredBeans = ko.computed(function() {
+    var filterBeans = this.filterBeans().toLowerCase();
+    if (!filterBeans) {
+        return this.beans();
     } else {
-        return ko.utils.arrayFilter(this.items(), function(item) {
-            return ko.utils.stringStartsWith(item.name.toLowerCase(), filter);
+        return ko.utils.arrayFilter(this.beans(), function(item) {
+            return ko.utils.stringStartsWith(item.name.toLowerCase(), filterBeans);
         });
     }
 }, viewModel);
